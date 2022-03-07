83c4da58743a0142fae6982bc4070af748860f2c
FL03 <jo3mccain@icloud.com>
2022-03-07 17:09:17 -0600

    Commit

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,11 +1,17 @@
+const { contextBridge, ipcRenderer } = require("electron");
 
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-        const element = document.getElementById(selector)
-        if (element) element.innerText = text
-    }
+contextBridge.exposeInMainWorld("darkMode", {
+  toggle: () => ipcRenderer.invoke("dark-mode:toggle"),
+  system: () => ipcRenderer.invoke("dark-mode:system")
+});
 
-    for (const type of ['chrome', 'node', 'electron']) {
-        replaceText(`${type}-version`, process.versions[type])
-    }
+window.addEventListener("DOMContentLoaded", () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  };
+
+  for (const type of ["chrome", "node", "electron"]) {
+    replaceText(`${type}-version`, process.versions[type]);
+  }
 })
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button id="toggle-dark-mode">Toggle Dark Mode</button>
+<button id="reset-to-system">Reset to System Theme</button>
