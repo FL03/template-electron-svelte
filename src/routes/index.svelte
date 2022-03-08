a6df15af3946f7f109ec969e27ebd8f075401858
FL03 <jo3mccain@icloud.com>
2022-03-07 21:57:27 -0600

    Commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module" lang="ts">
+  export const prerender = true;
+</script>
 
-<button id="toggle-dark-mode">Toggle Dark Mode</button>
-<button id="reset-to-system">Reset to System Theme</button>
+<script lang="ts">
+  import Counter from "$lib/Counter.svelte";
+</script>
+
+<svelte:head>
+  <title>Home</title>
+</svelte:head>
+
+<section>
+  <h1>
+    <div class="welcome">
+      <picture>
+        <source srcset="svelte-welcome.webp" type="image/webp" />
+        <img alt="Welcome" src="svelte-welcome.png" />
+      </picture>
+    </div>
+
+    to your new<br />SvelteKit app
+  </h1>
+
+  <h2>
+    try editing <strong>src/routes/index.svelte</strong>
+  </h2>
+
+  <Counter />
+</section>
+
+<style>
+    section {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        flex: 1;
+    }
+
+    h1 {
+        width: 100%;
+    }
+
+    .welcome {
+        position: relative;
+        width: 100%;
+        height: 0;
+        padding: 0 0 calc(100% * 495 / 2048) 0;
+    }
+
+    .welcome img {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        display: block;
+    }
+</style>
